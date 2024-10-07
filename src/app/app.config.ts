095abbf15e23cd48a4dfabec89588d9426fd3309
Kevin Jimenez <orlando.kj24@gmail.com>
2024-10-07 14:57:38 -0500

    feat: 🚧 store signals

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,16 +4,15 @@ import { provideRouter, withComponentInputBinding, withHashLocation } from '@ang
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { spinnerInterceptor } from './core';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideHttpClient(withInterceptors([spinnerInterceptor])),
-		provideRouter(
-			routes,
-			withHashLocation(),
-			withComponentInputBinding()
-			// withPreloading(PreloadAllModules)
-		)
-	]
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withInterceptors([spinnerInterceptor])),
+    provideRouter(routes, withHashLocation(), withComponentInputBinding()
+    // withPreloading(PreloadAllModules)
+    ),
+    provideStore()
+]
 };
